60f1997203c96acab1812a64feca2698760662e4
Tommy <Khattiya072@gmail.com>
2024-02-15 20:53:00 +0700

    Create Detail page

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Temple from './screen/Temple';
 import Home from './Screen/Home';
+import Detail from './Screen/Detail';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,11 @@ const App = () => {
           name="Home"
           component={Home}
           options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={Detail}
+          options={{ title: 'Detail'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ Screen/Detail.js
@@ -0,0 +1,75 @@
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import React from 'react';
+import data from './Attractions.json'
+
+const styles = StyleSheet.create({
+    BackgroundColor: {
+        backgroundColor: "#E9FFF0",
+        padding: 10
+    },
+
+    TopHeaderItem: {
+        TopHeader: "#000000",
+        alignItems: "center"
+    },
+
+    TopHeader: {
+        color: "#000",
+        fontSize: 35
+    },
+
+    Card: {
+        marginVertical: 20
+    },
+
+    Name: {
+        color: "#000000",
+        fontSize: 20,
+        marginTop: 10,
+        marginLeft: 10
+    },
+
+    Description: {
+        padding: 10
+    },
+
+    CoverImage: {
+        width: "100%",
+        height: 250,
+        marginVertical: 10
+    },
+    Address: {
+        fontSize: 4,
+        padding: 10,
+        marginBottom: 50
+    }
+})
+
+const Detail = ({ navigation, route }) => {
+    let d = data.find(o => o.id === route.params.id)
+    return (
+        <ScrollView style={styles.BackgroundColor}>
+            <View style={styles.Card}>
+                <Image style={styles.CoverImage} source={{ uri: d.coverImage }}/>
+                <Text style={styles.Name}>{ d.name }</Text>
+                <Text style={styles.Description}>{ d.deepDetail.subDetail1 }</Text>
+
+                <Image style={styles.CoverImage} source={{ uri: d.deepDetail.subImage1 }}/>
+                <Text style={styles.Description}>{ d.deepDetail.subDetail2 }</Text>
+
+                <Image style={styles.CoverImage} source={{ uri: d.deepDetail.subImage2 }}/>
+                <Text style={styles.Description}>{ d.deepDetail.subDetail3 }</Text>
+
+                <Image style={styles.CoverImage} source={{ uri: d.deepDetail.subImage3 }}/>
+                <Text style={styles.Description}>{ d.deepDetail.subDetail4 }</Text>
+            </View>
+            <View style={styles.Address}>
+                <Text>{ d.address.location }</Text>
+                <Text>{ d.address.open }</Text>
+                <Text>{ d.address.contract }</Text>
+            </View>
+        </ScrollView>
+    )
+}
+
+export default Detail
